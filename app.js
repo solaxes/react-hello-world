9524f4b7caf6d83ff70b3c1fd9f9c357c13f8506
Jaidev Bangar <solaxes@gmail.com>
2024-03-26 14:43:19 +0530

    Nested tags using react

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // Create Element here takes three arguments
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React!"
-);
+const heading = React.createElement("div", { id: "div1" }, [
+  React.createElement("div", { id: "dev1" }, [
+    React.createElement("h1", { id: "heading1" }, "Heading 1"),
+    React.createElement("h2", { id: "heading2" }, "heading 2"),
+  ]),
+  React.createElement("div", { id: "div2" }, [
+    React.createElement("h1", { id: "heading1" }, "Heading 1"),
+    React.createElement("h2", { id: "heading2" }, "Heading 2"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(heading);
